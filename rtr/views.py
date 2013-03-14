f489a96d1faae5b5366f0e5a71074584bf921838
TejasM <tejasmehta0@gmail.com>
2013-03-13 21:16:57 -0400

    Login set, and logic worked out

--- rtr/views.py
+++ rtr/views.py
@@ -3,12 +3,7 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import Group, Permission
-from django.contrib.contenttypes.models import ContentType
-
-content_type = ContentType.objects.get(app_label='rtr',model="Session")
-permissionCreater = Permission.objects.create(codename='started_sesssion',
-    name='Started Session',content_type=content_type)
+from rtr.models import Session
 
 
 def creater_check(user):
@@ -31,12 +26,16 @@ def login(request):
     if typeSession=="create":
         user = authenticate(username=username, password=password)
         if user is not None:
-            user.user_permissions.add(permissionCreater)
-            return HttpResponseRedirect(reverse("rtr:profsettings"))
+            try:
+                Session.objects.get(sessionId=session)
+                return render(request, 'rtr/index.html', {"error_message": "Session Id already in use please use another"})
+            except Session.DoesNotExist:
+                Session.objects.create(sessionId=session)
+                return HttpResponseRedirect(reverse("rtr:profsettings"))
         else:
-            return render(request, 'rtr/index.html', {"errorMessage": "Incorrect Username and/or Password"})
+            return render(request, 'rtr/index.html', {"error_message": "Incorrect Username and/or Password"})
     else:
         #Todo:
         user = authenticate(username="anon", password="anon")
         user.role = "joiner"
-        return render(request, 'rtr/index.html')
+        return render(request, 'rtr/index.html', {"error_message": "Cannot join session yet"})
