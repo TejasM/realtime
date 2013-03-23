0b329180c72d0b8a2a99ab928cff384918c55ec0
TejasM <tejasmehta0@gmail.com>
2013-03-23 14:25:50 -0400

    Debug true

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,4 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-     {'document_root': '/static'}),
 )
